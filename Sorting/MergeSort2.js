c988a067e7ca5c70fff2b92aca38edc900582187
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-09-13 13:16:43 +0530

    Merge Sort Practice

---
+++ Sorting/MergeSort2.js
@@ -0,0 +1,42 @@
+function mergteSort(arr, low, high) {
+  if (low == high) {
+    return;
+  }
+
+  let mid = Math.floor((low + high) / 2);
+  mergteSort(arr, low, mid);
+  mergteSort(arr, mid + 1, high);
+  merge(arr, low, mid, high);
+}
+
+function merge(arr, low, mid, high) {
+  let temp = [];
+  let left = low;
+  let right = mid + 1;
+
+  while (low <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push(arr[left]);
+      left++;
+    } else {
+      temp.push(arr[right]);
+      right++;
+    }
+  }
+
+  while (left <= mid) {
+    temp.push(arr[left]);
+    left++;
+  }
+  while (right <= high) {
+    temp.push(arr[right]);
+    right++;
+  }
+  for (let i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
+}
+
+let array = [7, 8, 9, 4, 8, 5, 6, 1, 2, 4];
+mergteSort(array, 0, array.length - 1);
+console.log(array);
