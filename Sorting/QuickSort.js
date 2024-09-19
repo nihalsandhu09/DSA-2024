8274cf95e3b5da1963182d0097d57ddf81ce9ed3
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-09-19 10:46:56 +0530

    QuickSort

---
+++ Sorting/QuickSort.js
@@ -0,0 +1,62 @@
+function partition(arr, low, high) {
+  const pivot = arr[low];
+  let i = low;
+  let j = high;
+
+  while (i < j) {
+    while (arr[i] <= pivot && i <= high) {
+      i++;
+    }
+    while (arr[j] > pivot && j > low) {
+      j--;
+    }
+    if (i < j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  [arr[low], arr[j]] = [arr[j], arr[low]];
+  return j;
+}
+
+function quickSort(arr, low, high) {
+  if (low < high) {
+    const pIndex = partition(arr, low, high);
+    quickSort(arr, low, pIndex - 1);
+    quickSort(arr, pIndex + 1, high);
+  }
+  return arr;
+}
+// const arr = [4, 6, 2, 5, 7, 9, 1, 3];
+// const length = arr.length - 1;
+// console.log(quickSort(arr, 0, length));
+
+function partition2(arr, low, high) {
+  const pivot = arr[low];
+  let i = low;
+  let j = high;
+
+  while (i < j) {
+    while (arr[i] <= pivot && i < high) {
+      i++;
+    }
+    while (arr[j] > pivot && j > low) {
+      j--;
+    }
+    if (i < j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  [arr[low], arr[j]] = [arr[j], arr[low]];
+  return j;
+}
+function quickSort(arr, low, high) {
+  if (low < high) {
+    const PIIndex = partition(arr, low, high);
+    quickSort(arr, low, PIIndex - 1);
+    quickSort(arr, PIIndex + 1, high);
+  }
+  return arr;
+}
+const arr = [4, 6, 2, 5, 7, 9, 1, 3];
+const length = arr.length - 1;
+console.log(quickSort(arr, 0, length));
